21872eecf09e52bdcd81858021e679da5ca203b4
fpswagg <essoungo@gmail.com>
2024-01-19 09:31:15 +0100

    Emptied the app.

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,7 @@
-import Body from "./reacts/Body";
-import Header from "./reacts/Header";
-import Footer from "./reacts/Footer";
-import { useState } from "react";
-
-
-import "./App.css";
-
 function App() {
-  const [page, setPage] = useState(0);
-
-  return (
-    <div>
-      <Header
-        page={page}
-        setPage={setPage}
-      />
-
-      <Body
-        page={page}
-        setPage={setPage}
-      />
-
-      <Footer />
-    </div>
-  )
+ return (<div>
+      
+  </div>)
 }
 
-export default App
+export default App;
